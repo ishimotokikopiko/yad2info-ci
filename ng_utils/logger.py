39fc380b788fef8f1284361b17781700028297b7
labtest216 <labtest216@gmail.com>
2025-01-07 23:33:09 +0200

    Add built wheel files

---
+++ ng_utils/logger.py
@@ -0,0 +1,12 @@
+import logging
+
+##########################################
+# Configure logging.
+##########################################
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+logger = logging.getLogger('mongodb_operations')
