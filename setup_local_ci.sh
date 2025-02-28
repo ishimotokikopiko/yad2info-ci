1f45988eb0fe3958af2fb81a8fd8b10633acd430
labtest216 <labtest216@gmail.com>
2025-02-28 13:27:03 +0200

    test

--- setup_local_ci.sh
+++ setup_local_ci.sh
@@ -9,6 +9,7 @@ echo "b13b784808359f31bc79b08a191f5f83757852957dd8fe3dbfcc38202ccf5768  actions-
 # Extract the installer
 tar xzf ./actions-runner-linux-x64-2.322.0.tar.gz
 
+
 # Create the runner and start the configuration experience
 ./config.sh --url https://github.com/ishimotokikopiko/yad2info-ci --token AQ5WZIUKBF7SL4TGM5FYHHTHYGTO2
 # Last step, run it!
